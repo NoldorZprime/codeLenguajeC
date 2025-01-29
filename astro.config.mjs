87faf4493a6bd4e3020c3c13eec02976760599ed
Alex Roel <alexroel@example.com>
2025-01-29 09:06:59 -0500

    codec-v1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,24 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import { sidebar } from './src/config/sidebar';
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'CodeC Lenguaje',
+			logo: {
+				src: './src/assets/code-c-logo.svg',
+				alt: 'CodeC Logo',
+			},
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+			customCss: [
+				'./src/styles/custom.css',
 			],
+			sidebar: sidebar,
 		}),
 	],
 });
---
+++ src/config/sidebar.ts
@@ -0,0 +1,14 @@
+export const sidebar = [
+    {
+        label: "Primeros pasos con C",
+        autogenerate: { directory: "01-get-started-with-c" },
+    },
+    {
+        label: "Fundamentos del lenguaje",
+        autogenerate: { directory: "02-fundamentals-of-the-language" },
+    },
+    {
+        label: "Estructuras de control",
+        autogenerate: { directory: "03-control-structure" },
+    },
+];
